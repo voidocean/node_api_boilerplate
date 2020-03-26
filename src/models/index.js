8440885d32022e40d4abad352cb21fc622b8beb3
Kanathip Chankitisakoon <bluenexuz@hotmail.com>
2020-03-26 10:14:59 +0700

    [add] squelize model handler indexjs

---
+++ src/models/index.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+
+const { DB_NAME, DB_HOST, DB_USERNAME, DB_PASSWORD } = require('../config')
+var basename = path.basename(__filename);
+var db        = {};
+
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {  
+  host: DB_HOST,
+  dialect: 'mysql',
+  logging: false,
+  operatorsAliases: false,
+  // dialectOptions: {
+  //     socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+  //     useUTC: true, 
+  // },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  define: {
+    paranoid: false
+  }
+});
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    var model = sequelize["import"](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
