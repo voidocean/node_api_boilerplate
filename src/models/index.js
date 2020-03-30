97d1cf4d776591626755554c95b82bcc8bb4461c
pichayuth <pichayuthkitti@gmail.com>
2020-03-30 16:41:52 +0700

    refactor config setting

--- src/models/index.js
+++ src/models/index.js
@@ -1,48 +1,48 @@
-"use strict";
+'use strict';
 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
+var fs = require('fs');
+var path = require('path');
+var Sequelize = require('sequelize');
 
-const { DB_NAME, DB_HOST, DB_USERNAME, DB_PASSWORD } = require('../config')
+const configs = require('../configs');
+const { DB_NAME, DB_HOST, DB_USERNAME, DB_PASSWORD } = configs.database;
+
 var basename = path.basename(__filename);
-var db        = {};
+var db = {};
 
-const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {  
-  host: DB_HOST,
-  dialect: 'mysql',
-  logging: false,
-  operatorsAliases: false,
-  // dialectOptions: {
-  //     socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
-  //     useUTC: true, 
-  // },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  define: {
-    paranoid: false
-  }
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+    operatorsAliases: false,
+    // dialectOptions: {
+    //     socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+    //     useUTC: true,
+    // },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+    define: {
+        paranoid: false,
+    },
 });
 
 fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
-  .forEach((file) => {
-    var model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+    .filter((file) => {
+        return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+    })
+    .forEach((file) => {
+        var model = sequelize['import'](path.join(__dirname, file));
+        db[model.name] = model;
+    });
 
 Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;
