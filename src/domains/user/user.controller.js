01fd75cf9fccb70f8ef33d994bb8872243303d0b
pichayuth <pichayuthkitti@gmail.com>
2020-03-30 16:42:09 +0700

    update import config

--- src/domains/user/user.controller.js
+++ src/domains/user/user.controller.js
@@ -1,12 +1,13 @@
-const { getUserByID } = require('./user.flows')
+const { getUserByID } = require('./user.flows');
+const configs = require('../../configs');
+const userConfig = configs.user;
 
 exports.getUser = async (req, res) => {
-    try {
-        const { id } = req.query
-        const response = await getUserByID(id);
-        return res.json(response)    
-    } catch (error) {
-        return res.status(500).json(error)
-    }
-    
-}
+  try {
+    const { id } = req.query;
+    const response = await getUserByID(id);
+    return res.json(response);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
