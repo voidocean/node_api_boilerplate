13538d769816a092c7f26b5337b2701a5c61a2c1
Kanathip Chankitisakoon <bluenexuz@hotmail.com>
2020-04-08 21:17:49 +0700

    [add] env config for database

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,34 @@
 module.exports = {
-    database: 'database_development',
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    options: {
-        host: process.env.DB_HOST || 'localhost',
-        dialect: 'mysql',
-        logging: false,
-    }
+    development: {
+        database: process.env.DB_SCHEMA || 'database_development',
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || 'root',
+        options: {
+            host: process.env.DB_HOST || 'localhost',
+            dialect: 'mysql',
+            logging: false,
+        }
+    },
+    test: {
+        database: process.env.DB_SCHEMA || 'database_development',
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || 'root',
+        options: {
+            host: process.env.DB_HOST || 'localhost',
+            dialect: 'mysql',
+            logging: false,
+        }
+    },
+    production: {
+        database: process.env.DB_SCHEMA || 'database_production',
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || 'root',
+        options: {
+            host: process.env.DB_HOST || 'localhost',
+            dialect: 'mysql',
+            logging: false,
+        }
+    },
+
+    
 };
