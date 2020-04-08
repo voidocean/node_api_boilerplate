03052fcde433414910409e79bb113c29583fcbcd
Kanathip Chankitisakoon <bluenexuz@hotmail.com>
2020-04-08 20:58:44 +0700

    [add] logout integration test

--- src/routes/authentication.routes.js
+++ src/routes/authentication.routes.js
@@ -1,9 +1,10 @@
-const { login, checkToken } = require('../domains/authentication/authentication.controller')
+const { login, checkToken, logout } = require('../domains/authentication/authentication.controller')
 const authentication_validator = require('../domains/authentication/authentication.validator')
 const { validationResponse } = require('../middlewares/validationResponse')
 const { auth_required } = require('../middlewares/authentication')
 module.exports = (app) => {
     app.post('/login', authentication_validator.validate('login'), validationResponse, login)
+    app.delete('/logout', auth_required, logout)
     app.get('/checktoken', auth_required, checkToken)
     
 }
--- test/integration/controllers/authentication.controller.in.test.js
+++ test/integration/controllers/authentication.controller.in.test.js
@@ -4,6 +4,7 @@ const new_user = require('../../mock-data/user/new_user.json')
 
 const loginEndpointURL = "/login/";
 const checkTokenEndpointURL = "/checkToken/";
+const logoutEndpointURL = "/logout/";
 const login_data = {email: 'john@email.com', password: 'password'}
 const wrong_login_data = {email: 'john@email.com', password: 'passwords'}
 let newToken;
@@ -35,6 +36,21 @@ describe(checkTokenEndpointURL, ()=>{
     it("should return 403 in get to "+ checkTokenEndpointURL, async () => {
         const response = await request(app)
             .get(checkTokenEndpointURL)
+            .set({'authorization': newToken+'1'})
+        expect(response.statusCode).toBe(403);
+    })
+})
+describe(logoutEndpointURL, ()=>{
+    it("should return token and 200 in get to "+ logoutEndpointURL, async () => {
+        const response = await request(app)
+            .delete(logoutEndpointURL)
+            .set({'authorization': "Bearer "+newToken})
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toStrictEqual({ message: 'sign out successful' })
+    })
+    it("should return 403 in get to "+ logoutEndpointURL, async () => {
+        const response = await request(app)
+            .delete(logoutEndpointURL)
             .set({'authorization': newToken+'1'})
         expect(response.statusCode).toBe(403);
     })
